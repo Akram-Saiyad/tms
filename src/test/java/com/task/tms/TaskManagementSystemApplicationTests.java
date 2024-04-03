22f3485a2f0b7b6681162b348b2624ba16346239
Akram Saiyad <saiyadak@pmc.local>
2024-04-03 17:39:00 +0530

    Initial commit

---
+++ src/test/java/com/task/tms/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.tms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
